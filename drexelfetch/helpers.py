2362a8d50bbebf9753eed28d97b39765b30e2fe1
dotzenith <sharmadanshu01@gmail.com>
2023-02-27 15:53:18 -0500

    fixed file imports?

--- drexelfetch/helpers.py
+++ drexelfetch/helpers.py
@@ -2,8 +2,7 @@
 A singular helper for DrexelFetch
 """
 
-import subprocess
-from pathlib import Path
+from importlib import resources
 
 import polars as pl
 
@@ -13,15 +12,7 @@ def get_courses_df() -> pl.DataFrame:
     Returns a polars DataFrame sourced from the courses.csv file
     """
 
-    # git_repo = (
-    #     subprocess.Popen(
-    #         ["git", "rev-parse", "--show-toplevel"], stdout=subprocess.PIPE
-    #     )
-    #     .communicate()[0]
-    #     .rstrip()
-    #     .decode("utf-8")
-    # )
-    #
-    # courses_path = Path(f"{git_repo}/courses.csv")
-
-    return pl.read_csv("courses.csv", sep=",")
+    with resources.as_file(
+        resources.files(__package__).joinpath("courses.csv")
+    ) as path:
+        return pl.read_csv(path, sep=",")
